faaa38d5f88f7682059a84ff4859a80e9ffec4ed
Ross Mawdsley <rossmawd@gmail.com>
2023-02-21 15:44:19 +0000

    removed redundant provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
 import HomePage from "./pages/home/HomePage";
 import AboutPage from "./pages/about/AboutPage";
 
 function App() {
   return (
-    <ChakraProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage></HomePage>}></Route>
-          <Route path="/about" element={<AboutPage />}></Route>
-        </Routes>
-      </Router>
-    </ChakraProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
