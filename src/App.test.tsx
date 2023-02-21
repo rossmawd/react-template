c9069ce990c59d9fd72282d119fc6d2082a88d01
Ross Mawdsley <rossmawd@gmail.com>
2023-02-21 16:19:02 +0000

    fixed unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,5 +5,5 @@ test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/About Page/i);
   console.log(linkElement);
-  expect(linkElement).toHaveTextContent("About Page");
+  expect(linkElement).toHaveAccessibleName("About page");
 });
